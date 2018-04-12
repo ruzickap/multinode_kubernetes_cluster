5e3df5efa9b581cfe98d792f235649db9a37811a
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-04-12 15:57:44 +0200

    Moving to Kubernetes 1.9.5

--- run-kubeadm.sh
+++ run-kubeadm.sh
@@ -4,9 +4,9 @@ MYUSER="vagrant"
 SSH_ARGS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 POD_NETWORK_CIDR="10.244.0.0/16"
 #KUBERNETES_VERSION="1.10.0"
-KUBERNETES_VERSION="1.9.0"
+KUBERNETES_VERSION="1.9.5"
 #CNI_URL="https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml"
-CNI_URL="https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml"
+CNI_URL="https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml"
 
 INSTALL_KUBERNETES="
 export DEBIAN_FRONTEND='noninteractive'
